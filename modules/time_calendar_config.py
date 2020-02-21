b49e2c43866c2610b96df873983182561a6f2382
Daniel Cruz <dftdcruz@gmail.com>
2020-02-21 12:27:03 -0300

    Inclusão de arquivos antigos na pasta, serão trabalhados...

---
+++ modules/time_calendar_config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#
+#  Copyright 2017 Daniel Cruz <bwb0de@bwb0dePC>
+#  Version 0.1
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+import os
+
+from modules.cli_tools import read_from_file, read_input, write_to_file, pick_options
+
+pasta_raiz = os.getcwd() + os.sep
+pasta_dados = 'data/'
+calendario_alvo_ref = pasta_raiz + '.target_cal_ref'
+
+
